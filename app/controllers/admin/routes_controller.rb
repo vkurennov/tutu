fc6f4bbe4c7b429062b85d9b5390deb33861ae21
Vitaly Kurennov <vkurennov@gmail.com>
2015-10-21 17:44:41 +0300

    Update paths for admin

--- app/controllers/admin/routes_controller.rb
+++ app/controllers/admin/routes_controller.rb
@@ -16,7 +16,7 @@ class Admin::RoutesController < Admin::BaseController
     @route = Route.new(route_params)
 
     if @route.save
-      redirect_to @route
+      redirect_to [:admin, @route]
     else
       render :new
     end
@@ -27,7 +27,7 @@ class Admin::RoutesController < Admin::BaseController
 
   def update
     if @route.update(route_params)
-      redirect_to @route
+      redirect_to [:admin, @route]
     else
       render :edit
     end
