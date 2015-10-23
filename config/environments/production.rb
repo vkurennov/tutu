f76b98021255f7e6d6b4762a145f0d54c6992142
Vitaly Kurennov <vkurennov@gmail.com>
2015-10-23 16:54:08 +0300

    Set host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'shielded-fjord-3541.herokuapp.com'}
 end
